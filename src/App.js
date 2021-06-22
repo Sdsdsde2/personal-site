f7c6d1f2bd6e8a9fb124ad9a19b3f8496c33a831
Sdsdsde2 <imspammails31@gmail.com>
2021-06-22 15:14:58 -0500

    changed css styling. added anime.js for animation on text

--- src/App.js
+++ src/App.js
@@ -1,35 +1,103 @@
+import React, { Component } from 'react';
 import './App.css';
+import pfp from './pfpexample.png';
+import anime from 'animejs/lib/anime.es.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-pfp">
+export default class App extends Component {
+  animationStart() {
+    for(var i = 1; i < 6; i++) {
+      // eslint-disable-next-line no-loop-func
+      function timer(){
+        var k = i;
+        setTimeout(()=>{
+          var textWrapper = document.querySelector(`#link-` + k);
+          textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+          var target = `#link-` + k
 
-        </div>
-        <div>
-          <h1 className="App-title">
-            Shane Possibly
-          </h1>
-          <h5 className="App-link">
-            Github
-          </h5>
-          <h5 className="App-link">
-            YouTube
-          </h5>
-          <h5 className="App-link">
-            Twitter
-          </h5>
-          <h5 className="App-link">
-            Linkedin
-          </h5>
-          <h5 className="App-link">
-            Medium
-          </h5>
-        </div>
-      </header>
-    </div>
-  );
+          anime.timeline()
+          .add({
+            targets: target,
+            scale: [4,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950,
+            delay: 285
+          })
+          .add({
+            targets: target,
+            translateX: 500,
+            autoplay: true,
+            duration: 1850,
+            delay: 15,
+            easing: 'easeInOutSine'
+          })
+        }, k * 750);
+      }
+      timer();
+    }
+  }
+
+  componentDidMount() {
+    this.animationStart();
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    // console.log(e)
+    // console.log(e.target.id)
+    // NOTE TO FUTURE SELF: When I click on the text i.e "Github" it doesn't redirect.
+    if (e.target.id === "link-1") {
+      window.location.href="https://github.com/Sdsdsde2"
+    }
+    
+    if (e.target.id === "link-2") {
+      window.location.href="https://github.com/Sdsdsde2"
+    }
+
+    if (e.target.id === "link-3") {
+      window.location.href="https://twitter.com/ShanePossibly"
+    }
+
+    if (e.target.id === "link-4") {
+      window.location.href="https://www.linkedin.com/in/shane-rachal-91013920a/"
+    }
+
+    if (e.target.id === "link-5") {
+      window.location.href="https://shanerpersonal.medium.com/"
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-pfp-boarder">
+            <img src={pfp} className="App-pfp"></img>
+          </div>
+          <div>
+            <h1 className="App-title">
+              Shane Possibly
+            </h1>
+            <h5 className="App-link" id="link-1" onClick={this.handleClick}>
+              Github
+            </h5>
+            <h5 className="App-link" id="link-2" onClick={this.handleClick}>
+              YouTube
+            </h5>
+            <h5 className="App-link" id="link-3" onClick={this.handleClick}>
+              Twitter
+            </h5>
+            <h5 className="App-link" id="link-4" onClick={this.handleClick}>
+              Linkedin
+            </h5>
+            <h5 className="App-link" id="link-5" onClick={this.handleClick}>
+              Medium
+            </h5>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
